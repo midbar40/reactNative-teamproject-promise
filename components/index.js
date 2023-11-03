f92d6b1ddb6fdc9251c5f499c2fa61c4e78ea274
Seunghyun <inyo0506@gmail.com>
2023-11-04 02:04:29 +0900

    학원파일과병합

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FindPassword } from './FindPassword';
-export { default as Logout } from './Logout';
-export { default as Login } from './Login';
-export { default as RegisterUser } from './RegisterUser';
--- screens/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HomeScreen } from './HomeScreen';
-export { default as CalendarScreen } from './CalendarScreen';
-export { default as AlarmScreen } from './AlarmScreen';
-export { default as TodoScreen } from './TodoScreen';
-export { default as LandingScreen } from './LandingScreen';
