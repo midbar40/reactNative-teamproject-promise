2e5abddf3c70eed272ca64acb4e4e09743f474a6
JeeGAe <eleldk82@naver.com>
2023-11-13 11:13:14 +0900

    푸쉬알림 하는 중

--- client/index.js
+++ client/index.js
@@ -6,5 +6,11 @@ import {AppRegistry} from 'react-native';
 // import App from './App';
 import stackRouter from './stackRouter';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+  // messaging().onMessage(remoteMessage)
+});
 
 AppRegistry.registerComponent(appName, () => stackRouter);
